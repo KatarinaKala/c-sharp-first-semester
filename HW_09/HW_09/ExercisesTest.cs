991988bc0f6436cec7956ea2367fc4f5f055feac
katkal <katkal@ttu.ee>
2019-07-18 21:29:02 +0300

    Started HW_09

---
+++ HW_09/HW_09/ExercisesTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace HW_09
+{
+    [TestFixture]
+    public class ExercisesTest
+    {
+        Exercises exercises;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            exercises = new Exercises();
+        }
+
+        [Test]
+        public void TestJoinTwoStrings() 
+        {
+            Assert.That(exercises.JoinTwoStrings("tere ", "hommikust"), Is.EquivalentTo("tere hommikust"));
+        }
+
+        [Test]
+        public void TestArrayLength()//checking that array consist of 6 integers
+        {
+            Assert.AreEqual(6, exercises.array.Length); 
+        }
+
+         [Test]
+         public void TestArrayContains100()//checking that array includes number 100
+        {
+            exercises.ArrayOfSixNr();
+            //Assert.That(100, Is.SubsetOf(exercises.array));
+            Assert.That(exercises.array, Has.Member(100));
+        }
+    }
+}
---
+++ HW_09/HW_09/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW_09
+{
+    class Program
+    {
+    }
+
+        class Exercises
+        {
+            public int[] array = new int[6]; //made an array for second exercise
+
+            public string JoinTwoStrings(string a, string b) //first method to add two strings
+            {
+                return a + b;
+            }
+
+            public int[] ArrayOfSixNr()
+            {
+                Random random = new Random();
+
+                array[0] = 100;
+                array[1] = random.Next(0, 21);
+                array[2] = random.Next(0, 21);
+                array[3] = random.Next(30, 61);
+                array[4] = random.Next(30, 61);
+                array[5] = random.Next(100, 201);
+
+
+                foreach (int nr in array)
+                {
+                    Console.WriteLine(nr);
+                }
+
+                return array; ;
+            }
+        }
+
+}
